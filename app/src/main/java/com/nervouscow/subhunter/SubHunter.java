cbba959b492ed3961faed5a359f8de254effac8c
barnsoir <barney.lewis@gmail.com>
2018-12-24 15:34:29 +0000

    Initial check in

---
+++ app/src/main/java/com/nervouscow/subhunter/SubHunter.java
@@ -0,0 +1,12 @@
+package com.nervouscow.subhunter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubHunter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
